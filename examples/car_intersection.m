1ed2d755874fc0366073be32ba4a2768dc730364
Nikhil Gupta <nikhil97gupta@gmail.com>
2018-06-01 02:48:56 -0700

    Progress on avoiding a car on yield
    Running into an issue where the car will violate our safety
    condition. I suspect there is a parameter for hard and soft
    requirements.

--- examples/car_intersection.m
+++ examples/car_intersection.m
@@ -11,29 +11,43 @@ SC = car_intersection_class();
 % x, y
 SC.x0= [0.5 ; -3];
 
-max_time = 20;
+max_time = 30;
+ts = 0.5;
 
-SC.time = 0:.5:max_time; 
+SC.time = 0:ts:max_time; 
 SC.ts=1; % sampling time for controller
 SC.L=10;   % horizon
 
-% can move either up or left or right
-SC.u_ub = [1; 1];  % upper bound on u 
+% can move either up or left
+SC.u_ub = [0; 1];  % upper bound on u 
 SC.u_lb = [-1; 0]; % lower bound on u
-% Sys.u_delta = [1; 1]; % not really sure what this does
+Sys.u_delta = [.1; .1];
 
+% first  row is x coordinate of center vehicle
+% second row is y coordinate of center vehicle
 SC.Wref = [SC.time*0.; SC.time*0.];
+
+% default position of vehicle is off screen
+SC.Wref(:,:) = -10;
+
+% stationary test
+SC.Wref(1,:) = -0.5;
+SC.Wref(2,1:10/ts) = linspace(3,-3,10/ts);
 
 SC.stl_list = {};
 
 % goal, make sure we are in our lane and past the intersection
-SC.stl_list(end+1) = {'ev (x1(t) < -2 and x2(t) < 1 and x2(t) > 0)'};
+SC.stl_list(end+1) = {'ev (alw (x1(t) < -2))'};
 
 % safety, make sure we are always in our lane
 SC.stl_list(end+1) = {'and alw ((x1(t) > 0 and x1(t) < 1) or (x2(t) > 0 and x2(t) < 1))'};
 
 % make sure we don't hit another car
-% Sys.stl_list(end+1) = {'alw (abs(x1(t) - w1(t)) > 0.5 or abs(x2(t) - w2(t) > 0.5))'};
+% the other vehicle is 0.5*2 units wide of 2*2 units long
+% build in a buffer of 0.1
+x_size = 0.3;
+y_size = 0.6;
+SC.stl_list(end+1) = {'and alw (abs(x1(t) - w1(t)) > 0.4 or abs(x2(t) - w2(t)) > 0.7)'};
 
 SC.stl_list = {strjoin(SC.stl_list)};
 
@@ -45,11 +59,14 @@ fprintf("running...\n");
 SC= SC.run_deterministic(controller);
 
 %% Plot the path of the car over time
+close;
 figure;
 
 h = animatedline('linewidth', 10);
 axis([-3 3 -3 3]);
 set(gca, 'fontsize', 25);
+xlabel('x1');
+ylabel('x2');
 
 hold on;
 
@@ -67,14 +84,34 @@ plot([0 0], [-3 3], '--', 'LineWidth', 10, 'Color', 'k');
 % horizonal lane lines
 plot([-3 3], [0 0], '--', 'LineWidth', 10, 'Color', 'k');
 
-for i=1:max_time+1
+for i=1:size(SC.system_data.W,2)
     hold on;
     title(['time=' num2str(SC.time(i))], 'fontsize', 30);
+    
+    x_coor = SC.system_data.W(1, i) - x_size;
+    y_coor = SC.system_data.W(2, i) - y_size;
+    width  = x_size*2;
+    height = y_size*2;
+        
+    r = rectangle('Position', [x_coor y_coor width height], 'LineWidth', 3, 'FaceColor', [0 0 0 0.5]);
+    
     h2 = plot( SC.system_data.X(1, i),  SC.system_data.X(2, i), '^r-', 'MarkerSize', 25, 'MarkerFaceColor', 'r');
     addpoints(h, SC.system_data.X(1, i), SC.system_data.X(2, i));
     drawnow;
     
-    pause(.05);
+    if SC.time(i) == 5
+        pause(10)
+    end
+    pause(.5);
     delete(h2);
+    delete(r);
+    
 end
+x_coor = SC.system_data.W(1, i) - x_size;
+y_coor = SC.system_data.W(2, i) - y_size;
+width  = x_size*2;
+height = y_size*2;
+
+r = rectangle('Position', [x_coor y_coor width height], 'LineWidth', 3, 'FaceColor', [0 0 0 0.5]);
+
 plot( SC.system_data.X(1, i),  SC.system_data.X(2, i), '^r-', 'MarkerSize', 25, 'MarkerFaceColor', 'r');
--- examples/car_intersection_class.m
+++ examples/car_intersection_class.m
@@ -23,7 +23,7 @@ classdef car_intersection_class < STLC_lti
             % last term penalizes inputs late in execution
             % https://yalmip.github.io/command/sdpfun/
             % a = sdpfun(find(max(abs(U))),'max');
-            %obj = norm(sum(abs(U),2), Sys.nrm)-wr*norm(sum(rho,2), Sys.nrm) + a;
+            %obj = norm(sum(abs(U),2), Sys.nrm)-wr*norm(sum(rho,2), Sys.nrm);
             obj = norm(sum(abs(U),2), Sys.nrm)-wr*norm(sum(rho,2), Sys.nrm) + max(find(sum(abs(double(U))>0.1)));
         end  
         
